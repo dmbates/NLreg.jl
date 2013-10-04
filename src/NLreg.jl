91ce3519206003e0d8a5944e0b935dd07172b5d7
Douglas Bates <dmbates@gmail.com>
2013-10-04 16:58:46 -0500

    Got the pnls function working, still having problems with fit function

--- src/NLreg.jl
+++ src/NLreg.jl
@@ -1,6 +1,6 @@
 module NLreg
 
-    using DataFrames, NumericExtensions
+    using DataFrames, NumericExtensions, NLopt
     using Base.LinAlg.BLAS: gemv!, trmm!, trsm!, trsv!, syrk!
     using Base.LinAlg.LAPACK: gemqrt!,geqrt3!, potri!, potrf!, potrs!
     using Base.LinAlg.CHOLMOD: CholmodSparse!, CholmodSparse, CholmodFactor
@@ -13,7 +13,7 @@ module NLreg
     import Stats: coef, coeftable, confint, loglikelihood, nobs, stderr, vcov,
                   model_response, predict, residuals, rle
 
-    export                              # types
+    export
         AsympReg,      # Asymptotic regression model
         MicMen,        # Michaelis-Menten model
         NLregMod,      # Nonlinear regression model abstract type
@@ -23,13 +23,18 @@ module NLreg
         SimpleNLMM,    # Simple population nonlinear mixed-effects model
         Logsd1,        # 1-compartment, single bolus dose model with logged parameters
 
+        deviance,      # Laplace approximation to the deviance of an NLMM
         gnfit,         # nonlinear least squares by Gauss-Newton
         gpinc,         # Golub-Pereyra increment
         incr!,         # increment the spherical random effects
         initpars,      # create initial values for the parameters
+        lowerbd,       # lower bounds on NLMM parameters
         pnames,        # names of parameters in a model
         pnls!,         # penalized nonlinear least squares fit
         prss!,         # penalized rss for b = lambda * (u + fac*delu)
+        setpars!,      # set new parameter values (beta + theta) in an NLMM
+        theta,         # extract covariance parameters
+        theta!,        # set covariance parameters
         updtMM!,       # update the model matrix in a PLregMod
         updtL!,        # update L and solve for delu
         updtmu!        # update mu and tgrad
@@ -39,6 +44,6 @@ module NLreg
     include("nlreg.jl")
     include("plreg.jl")
     include("models.jl")
-    include("poppk.jl")
+    include("nlmm.jl")
 
 end # module
--- src/nlmm.jl
+++ src/nlmm.jl
@@ -1,6 +1,6 @@
-abstract NLMM <: StatisticalModel
+abstract NLMM{T<:FP} <: StatisticalModel
 
-type SimpleNLMM{T<:FP} <: NLMM
+type SimpleNLMM{T<:FP} <: NLMM{T}
     m::NLregMod{T}
     inds::Vector
     nrep::Vector
@@ -11,6 +11,7 @@ type SimpleNLMM{T<:FP} <: NLMM
     delu::Matrix{T}       # increment in the PNLS algorithm
     b::Matrix{T}          # random effects on original scale
     phi::Matrix{T}        # phi := beta + lambda * (u + fac*delu)
+    nAGQ::Int             # number of quadrature points in adaptive Gauss-Hermite
     mxpnls::Int           # maximum number of PNLS iterations
     minfac::T
     tolsqr::T
@@ -23,7 +24,7 @@ function SimpleNLMM{T<:FP}(m::NLregMod{T},inds::Vector,lambda::AbstractMatrix{T}
     size(lambda) == (p,p) || error("size(lambda) = $(size(lambda)) should be $((p,p))")
     L = Matrix{T}[eye(T,p) for i in 1:ni]
     u = zeros(T,(p,ni))
-    SimpleNLMM(m,inds,nrep,lambda,L,beta,u,copy(u),similar(u),similar(u),30,convert(T,0.5^9),convert(T,1e-8))
+    SimpleNLMM(m,inds,nrep,lambda,L,beta,u,copy(u),similar(u),similar(u),1,30,convert(T,0.5^9),convert(T,1e-8))
 end
 ## ToDo Add an external constructor using Formula/Data
 
@@ -74,19 +75,90 @@ function incr!{T<:FP}(nm::SimpleNLMM{T}, fac::T)
     fill!(nm.delu,zero(T))
 end
 
+## evaluate the approximate deviance using adaptive Gauss-Hermite quadrature
+function deviance{T<:FP}(nm::SimpleNLMM{T})
+    nm.nAGQ == 1 || error("General adaptive Gauss-Hermite quadrature not yet written")
+    p,n = size(nm.m)
+    ldL2(nm) + n * (one(T) + log(2pi*prss!(nm)/n))
+end
+
 ## Determine the conditional mode of the random effects using penalized nonlinear least squares
-function pnlsfit!{T<:FP}(nm::SimpleNLMM{T})
-    m = nm.m; id = nm.inds; RX = nm.RX; lam = nm.lambda; L = nm.L; u = nm.u; delu = nm.delu
-    oldprss = prss!(nm,0.); conv = one(T)
-    for i in 1:nm.mxpnlsit
+function pnls!{T<:FP}(nm::SimpleNLMM{T})
+    fill!(nm.u,zero(T)); fill!(nm.delu,zero(T)) # start from a fixed position
+    oldprss = prss!(nm,0.); conv = one(T);
+    for i in 1:nm.mxpnls
         newprss = updtL!(nm); f = one(T)
         while oldprss < newprss && f >= nm.minfac
             f /= convert(T,2)
             newprss = prss!(nm,f)
         end
         f < nm.minfac && error("Failure to reduce prss at u = ",nm.u)
-        incr!(pp,f)
+        incr!(nm,f)
         (conv = (oldprss - newprss)/newprss) < nm.tolsqr && break
+        oldprss = newprss
     end
     conv >= nm.tolsqr && error("Failure to converge in ", nm.mxpnlsit, " iterations")
+    deviance(nm)
+end
+
+theta(nm::NLMM) = theta(nm.lambda)
+theta(lambda::Diagonal) = lambda.diag
+function theta(m::Triangular)
+    n = size(m,1); v = Array(eltype(T),n*(n+1)>>1); pos = 0; UL = m.UL
+    for j in 1:n, i in (m.uplo == 'L' ? (j:n) : (1:j)) v[pos += 1] = UL[i,j] end
+    v
+end
+
+theta!{T}(lambda::Diagonal{T},th::Vector{T}) = lambda.diag = th
+function theta!{T}(m::Triangular{T},th::Vector{T})
+    n = size(m,1); pos = 0; UL = m.UL
+    length(th) == n*(n+1)>>1 || error("length(th) = $(length(th)), should be $(n*(n+1)/2)")
+    for j in 1:n, i in (m.uplo == 'L' ? (j:n) : (1:j)) UL[i,j] = th[pos += 1] end
+    m
+end
+
+lowerbd{T<:FP}(nm::NLMM{T}) = [fill(-inf(T),length(nm.beta)),lowerbd(nm.lambda)]
+lowerbd{T}(lambda::Diagonal{T}) = zeros(T,size(lambda,1))
+function lowerbd{T}(m::Triangular{T})
+    n = size(m,1); v = fill(-inf(T),n*(n+1)>>1); pos = 1; zt = zero(T)
+    for j in 1:n, i in (m.uplo == 'L' ? (j:n) : (1:j))
+        i == j && (v[pos] = zt)
+        pos += 1
+    end
+    v
+end
+
+function setpars!{T<:FP}(nm::NLMM{T},pars::Vector{T})
+    bb = nm.beta; p = length(bb); copy!(bb, sub(pars,1:p))
+    theta!(nm.lambda,pars[(p+1):end])
+    nm
+end
+    
+function fit(nm::NLMM, verbose=false)
+    th = theta(nm); nth = length(th)
+    pars = [nm.beta,th];
+    opt = Opt(:LN_BOBYQA, length(pars))
+    ftol_abs!(opt, 1e-6)    # criterion on deviance changes
+    xtol_abs!(opt, 1e-6)    # criterion on all parameter value changes
+    lower_bounds!(opt, lowerbd(nm))    
+    function obj(x::Vector{Float64}, g::Vector{Float64})
+        length(g) == 0 || error("gradient evaluations are not provided")
+        pnls!(setpars!(nm,x))
+    end
+    if verbose
+        count = 0
+        function vobj(x::Vector{Float64}, g::Vector{Float64})
+            if length(g) > 0 error("gradient evaluations are not provided") end
+            count += 1
+            val = obj(x, g)
+            println("f_$count: $val, $x")
+            val
+        end
+        min_objective!(opt, vobj)
+    else
+        min_objective!(opt, obj)
+    end
+    fmin, xmin, ret = optimize(opt, pars)
+    if verbose println(ret) end
+    nm
 end
