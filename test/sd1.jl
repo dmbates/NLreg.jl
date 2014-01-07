2809c9a3a65485c57b7476f3359d692410944f8f
Douglas Bates <dmbates@gmail.com>
2014-01-07 17:01:17 -0600

    Revise model name and use current parameter estimates for comparison.

--- test/sd1.jl
+++ test/sd1.jl
@@ -3,6 +3,6 @@ using DataFrames, NLreg
 
 const sd1 = within(readtable(Pkg.dir("NLreg","data","sd1.csv.gz")),:(ID = pool(ID)))
 
-nl = NonlinearLS(logsd1(:(CONC ~ TIME), sd1))
+nl = NonlinearLS(Logsd1(:(CONC ~ TIME), sd1))
 
-@test_approx_eq coef(nl) [0.13355908560885243,-1.4038489231938387]
+@test_approx_eq coef(nl) [0.1336302830419119,-1.4037023962519823]
