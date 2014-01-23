26fa0bf313d87bc7670a113983ac797eb53eddb2
Douglas Bates <dmbates@gmail.com>
2014-01-23 15:23:05 -0600

    gnfit is no longer exported, use the PLregMod instead.

--- test/sd1.jl
+++ test/sd1.jl
@@ -1,12 +1,12 @@
 using Base.Test
 using DataFrames, NLreg
 
-const sd1 = within(readtable(Pkg.dir("NLreg","data","sd1.csv.gz")),:(ID = pool(ID)))
+const sd1 = within(readtable(Pkg.dir("NLreg","data","sd1.csv.gz")),:(ID = pool(ID)));
 
-nl = gnfit(NonlinearLS(Logsd1(:(CONC ~ TIME), sd1)),true)
+# nl = gnfit(NonlinearLS(Logsd1(:(CONC ~ TIME), sd1)),true)
 
-pl = gpfit(PLinearLS(LogBolusSD1(:(CONC ~ TIME), sd1)),true)
+pl = fit(LogBolusSD1(CONC ~ TIME, sd1),true)
 
-@test_approx_eq coef(nl) [0.1336302830419119,-1.4037023962519823]
+@test_approx_eq coef(pl) [1.1429558452268844,-1.4036989045425874]
 
 pl = fit(AsympReg([2.:7],[18.6,22.6,25.1,27.2,29.1,30.1]),true)
