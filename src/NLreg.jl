5c2430cc5efd336867918f12ae819c7e1e3a8c87
Douglas Bates <dmbates@gmail.com>
2014-02-19 15:47:16 -0600

    Move CoefTable type to StatsBase.

--- src/NLreg.jl
+++ src/NLreg.jl
@@ -2,15 +2,14 @@ module NLreg
 
     using DataArrays, DataFrames, NumericExtensions, NLopt, Base.Grisu
     using Base.LinAlg.CHOLMOD: CholmodSparse!, CholmodSparse, CholmodFactor
-    using StatsBase: StatisticalModel, RegressionModel
+    using StatsBase: CoefTable, StatisticalModel, RegressionModel, range
     using Distributions: FDist, ccdf
-    using Base.Grisu: _show, PRECISION, FIXED, SHORTEST
     using Base.LinAlg.QRCompactWY
 
     importall Base
     import Distributions: fit
-    import StatsBase: coef, coeftable, confint, deviance, loglikelihood, nobs, stderr, vcov,
-                  model_response, predict, residuals, rle
+    import StatsBase: coef, coeftable, confint, deviance, loglikelihood, nobs,
+               stderr, vcov, model_response, predict, residuals, rle
 
     export     # Partially linear models
         AsympOff,     # Asymptotic regression expressed as an offset
@@ -57,62 +56,6 @@ module NLreg
         updtmu!       # update mu and tgrad
 
     typealias FP FloatingPoint
-
-    type CoefTable
-        df::DataFrame
-        nms::Vector
-        pvalcol::Integer
-        function CoefTable(df::DataFrame,nms::Vector,pvalcol::Int=0)
-            nr,nc = size(df)
-            cnms = names(df)
-            nnms = length(nms)
-            0 <= pvalcol <= nc || error("pvalcol = $pvalcol should be in [0,$nc]")
-            nnms == 0 || nnms == nr || error("nms should have length 0 or $nr")
-            new(df,nms,pvalcol)
-        end
-    end
-
-    function format_pvc(pv::Number)
-        0. <= pv <= 1. || error("p-values must be in [0.,1.]")
-        pv >= eps() || return "< eps()"
-        (expo = ifloor(log10(pv))) >= -3 && return sprint(_show,pv,FIXED,4,true)
-        sprint(_show,pv,PRECISION,2,true)
-    end
-
-    function show(io::IO, ct::CoefTable)
-        df = ct.df; nr,nc = size(df); rownms = ct.nms; pvc = ct.pvalcol
-        if length(rownms) == 0
-            rownms = [lpad("[$i]",ifloor(log10(nr))+3)::String for i in 1:nr]
-        end
-        rnwidth = max(4,maximum([length(nm) for nm in rownms]) + 1)
-        rownms = [rpad(nm,rnwidth) for nm in rownms]
-        colnms = names(df)
-        widths = [length(cn)::Int for cn in colnms]
-        str = [sprint(showcompact,df[i,j]) for i in 1:nr, j in 1:nc]
-        if pvc != 0                         # format the p-values column
-            for i in 1:nr
-                str[i,pvc] = format_pvc(df[i,pvc])
-            end
-        end
-        for j in 1:nc
-            for i in 1:nr
-                lij = length(str[i,j])
-                if lij > widths[j]
-                    widths[j] = lij
-                end
-            end
-        end
-        widths += 1
-        println(io," " ^ rnwidth *
-            join([lpad(string(colnms[i]), widths[i]) for i = 1:nc], ""))
-        for i = 1:nr
-            print(io, rownms[i])
-            for j in 1:nc
-                print(io, lpad(str[i,j],widths[j]))
-            end
-            println(io)
-        end
-    end
 
     include("nonlinreg.jl")
     include("plreg.jl")
